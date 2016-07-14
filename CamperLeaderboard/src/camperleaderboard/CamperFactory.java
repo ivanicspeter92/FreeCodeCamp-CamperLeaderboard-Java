592cac3fe0f6504d35980d0d339b3add3cedd264
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-14 23:52:34 +0300

    Added implementation for getCampers(String json)

--- CamperLeaderboard/src/camperleaderboard/CamperFactory.java
+++ CamperLeaderboard/src/camperleaderboard/CamperFactory.java
@@ -6,6 +6,9 @@
 package camperleaderboard;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -15,6 +18,28 @@ public class CamperFactory {
     public static ArrayList<Camper> getCampers(String json) {
         ArrayList<Camper> campers = new ArrayList();
         
+        JSONArray camperObjects = new JSONArray(json);
+        for (int i = 0; i < camperObjects.length(); i++) {
+            JSONObject camperObject = camperObjects.getJSONObject(i);
+            Camper thisCamper = CamperFactory.unwrapCamper(camperObject);
+            
+            if (thisCamper != null) 
+                campers.add(thisCamper);
+        }
         return campers;
+    }
+    
+    private static Camper unwrapCamper(JSONObject fromObject) {
+        try {
+            String username = fromObject.getString("username");
+            String img = fromObject.getString("img");
+            int alltime = fromObject.getInt("alltime");
+            int recent = fromObject.getInt("recent");
+            String lastUpdate = fromObject.getString("lastUpdate");
+        
+            return new Camper(username, img, alltime, recent, lastUpdate);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
