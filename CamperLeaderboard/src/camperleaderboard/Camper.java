1d84dae11fb24cce1e2c839856d6322e205dff22
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-14 23:52:16 +0300

    Added new constructor

--- CamperLeaderboard/src/camperleaderboard/Camper.java
+++ CamperLeaderboard/src/camperleaderboard/Camper.java
@@ -13,12 +13,19 @@ public class Camper
     int recent;
     Calendar lastUpdate;
 
-public Camper(String username, String img, int alltime, int recent, Calendar lastUpdate){
-    this.username = username;
-    this.img = img;
-    this.alltime = alltime;
-    this.recent = recent;
-    this.lastUpdate = lastUpdate;
-}
-
+    public Camper(String username, String img, int alltime, int recent, Calendar lastUpdate){
+        this.username = username;
+        this.img = img;
+        this.alltime = alltime;
+        this.recent = recent;
+        this.lastUpdate = lastUpdate;
+    }
+    
+    public Camper(String username, String img, int alltime, int recent, String lastUpdate){
+        this.username = username;
+        this.img = img;
+        this.alltime = alltime;
+        this.recent = recent;
+//        this.lastUpdate = lastUpdate;
+    }
 }
