5e7380c4e63841cab8cca7aa4c6a89a661963598
Artemklimenkode <Artemklimenko96@gmail.com>
2016-07-14 21:39:07 +0300

    Created Camper class with the fields

---
+++ CamperLeaderboard/src/camperleaderboard/Camper.java
@@ -0,0 +1,17 @@
+package camperleaderboard;
+
+import java.util.Calendar;
+
+/**
+ * Created by Artem Klimenko on 14/07/2016.
+ */
+public class Camper
+{
+    String username;
+    String img;
+    int alltime;
+    int recent;
+    Calendar lastUpdate;
+
+
+}
